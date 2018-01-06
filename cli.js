9459eb8b1fd4d3a00bcaf697a37c4c3bbab856e6
Jeff Allen <1063272+jeffallen6767@users.noreply.github.com>
2018-01-05 23:15:46 -0800

    add compare option and colored output, fix file and message options

--- cli.js
+++ cli.js
@@ -1,27 +1,61 @@
 #!/usr/bin/env node
 var sha256 = require("./index"),
+  colors = require("colors"),
 	fs = require('fs'),
   utf8 = require('utf8'),
   program = require('commander'),
   pkg = require('./package.json'),
-  version = pkg.version;
+  version = pkg.version,
+  message,
+  filePath,
+  compare,
+  result,
+  done;
   
 program
   .version(version)
-  .option('-f, --file', 'file to apply sha-256')
+  .option('-m, --message [text]', 'text to apply sha-256')
+  .option('-f, --file [path]', 'file to apply sha-256')
+  .option('-c, --compare [checksum]', 'Checksum to compare generated with')
   .parse(process.argv);
 
-if (program.file) {
-  var filePath = program.args[0],
-    message = fs.readFileSync(filePath),
-    result = sha256(message);
-  console.log(result);
-} else if (program.args.length === 1) {
-  var message = program.args[0],
-    result = sha256(
-      utf8.encode(message)
+message = program.message;
+filePath = program.file;
+compare = program.compare;
+
+done = function(result) {
+  if (compare) {
+    if (compare.toLowerCase() === result.toLowerCase()) {
+      console.log(
+        colors.green.bold(result)
+      );
+    } else {
+      console.log(
+        colors.red.bold(result)
+      );
+    }
+  } else {
+    console.log(
+      colors.white.bold(result)
     );
-  console.log(result);
+  }
+};
+
+if (filePath) {
+  done(
+    sha256(
+      fs.readFileSync(filePath)
+    )
+  );
+} else if (message) {
+  done(
+    sha256(
+      utf8.encode(message)
+    )
+  );
 } else {
-  console.log('ERROR: Unknown program', program);
+  console.log(
+    colors.white.bold('ERROR: Must use with either -m [text] or -f [path]')
+  );
+  program.help();
 }
